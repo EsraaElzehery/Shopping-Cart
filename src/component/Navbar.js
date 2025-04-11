38f0f0db457f442a0774e391751001bb721ffbe5
Esraa <esraaelsaid2525@gmail.com>
2025-04-11 23:53:59 +0200

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -11,9 +11,7 @@ function Navbar() {
     <NavbarBs sticky='top' className='bg-white shadow-sm mb-5'>
 <Container>
 <Nav className='me-auto'>
-// <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
 <Nav.Link to="/store" as={NavLink}>Store</Nav.Link>
-// <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
 </Nav>
 
 <Button onClick={()=> openCart()} style={{width: 40, height:40, position:"relative" ,display: 'flex', 
